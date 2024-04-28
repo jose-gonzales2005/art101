1aef5c37a7081e337b8af8c11af533772459981c
jose-gonzales2005 <pineapple8411@gmail.com>
2024-04-27 23:43:35 -0700

    lab6 stuffff

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,16 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - time to coding 
+// Author: Noah Dane 
+// Date: 
 
-// Constants
+let myTransport = ["2002 Highlander", " Bike", " my feet ^_^ "]
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+const myMainRide = {
+  make: "Toyota",
+  model: "Highlander",
+  color: "blueish",
+  year: "2003",
+  age: function() {
+    return 2024 - this.year 
+  }
 }
+
+/*I am assuming when doing this that when we are told reference the elements of 
+both myTransport and myMainRide that it be only necessary to reference them once in our console log*/
+console.log(myTransport[2], myMainRide.color)
+
+document.writeln("Kinds of transportation I use:", myTransport, "</br>"); 
+
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 function main() {
   console.log("Main function started.");
