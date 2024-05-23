d313513221224bcf7f8d13a009763c2f622f5aab
jose-gonzales2005 <pineapple8411@gmail.com>
2024-05-23 16:26:01 -0700

    lab 12, and maybe lab 11 too

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,5 +1,31 @@
 /* Author: Noah Dane, Syd Young 
-lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page*/
+
+lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+ Requirements: jQuery must be loaded for this script to work.
+
+Date: Thur, May 16, 2024
+
+*/
+
+function sortString(inputString) {
+    return inputString.split('').sort().join('');
+}
 
 
 
+$("#submit").click(function () {
+    const userName = $("#user-name").val();
+
+    userNameSorted = sortString(userName);
+
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
+
+$("#submit").click(function () {
+    const userName = $("#user-name").val();
+
+    userNameSorted = sortString(userName);
+
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
